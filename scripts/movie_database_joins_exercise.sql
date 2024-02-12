5b43bf341bf54bf04ef0e85696c629fa60b9ea97
Tim Spratt <timothyspratt88@gmail.com>
2024-02-12 16:10:26 -0600

    finishing questions

--- scripts/movie_database_joins_exercise.sql
+++ scripts/movie_database_joins_exercise.sql
@@ -30,18 +30,34 @@ ON specs.domestic_distributor_id = distributors.distributor_id
 WHERE mpaa_rating = 'G'
 ORDER BY worldwide_gross DESC
 
---Answer: The highest grossing G-rated movie is Toy Story 4. Walt Disney is the company that released it.
+--Answer: The highest grossing G-rated movie is Toy Story 4. Walt Disney is the company that distributed it.
 
 
 --4. Write a query that returns, for each distributor in the distributors table, the distributor name and the number of movies associated with that distributor in the movies table. Your result set should include all of the distributors, whether or not they have any movies in the movies table.
 
-SELECT
-FROM
+SELECT DISTINCT(company_name), COUNT(film_title) AS number_of_films
+FROM distributors
+INNER JOIN specs
+ON distributors.distributor_id = specs.domestic_distributor_id
+GROUP BY company_name
+ORDER BY number_of_films DESC
+
+--Answer: The number one answer is Walt Disney with 76 movies
 
 --5. Write a query that returns the five distributors with the highest average movie budget.
 
-SELECT
-FROM
+SELECT company_name, AVG(film_budget) AS film_budget
+FROM distributors
+INNER JOIN specs
+ON distributors.distributor_id = specs.domestic_distributor_id
+INNER JOIN revenue
+ON specs.movie_id = revenue.movie_id
+GROUP BY company_name
+ORDER BY film_budget DESC
+LIMIT 5;
+
+--Answer: The 5 distributors with the highest average movie budget are: Walt Disney, Sony Pictures, Lionsgate, Dreamworks, and Warner Bros.
+
 
 --6. How many movies in the dataset are distributed by a company which is not headquartered in California? Which of these movies has the highest imdb rating?
 
